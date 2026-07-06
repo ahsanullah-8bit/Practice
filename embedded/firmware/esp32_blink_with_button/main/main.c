dd9872f115b62687fe555eb6ef06a53d5491787b
Ahsan Ullah <hatrakhan@gmail.com>
2026-07-06 09:25:34 +0500

    Preserve: Somewhat functional long and double click switching

--- embedded/firmware/esp32_blink_with_button/main/main.c
+++ embedded/firmware/esp32_blink_with_button/main/main.c
@@ -1,31 +1,49 @@
+#include "soc/gpio_num.h"
 #include <driver/gpio.h>
 #include <freertos/idf_additions.h>
 #include <hal/gpio_types.h>
 #include <portmacro.h>
 #include <esp_log.h>
 #include <stdint.h>
+#include <sys/types.h>
 
 void app_main(void)
 {
     const char *TAG = "BlinkWithButton";
-    const uint8_t GPIO_BUTTON = 22;
-    const uint8_t GPIO_LED = 23;
+    const gpio_num_t GPIO_BUTTON = GPIO_NUM_22;
+    const gpio_num_t GPIO_LED = GPIO_NUM_23;
 
     gpio_reset_pin(GPIO_BUTTON);
     gpio_reset_pin(GPIO_LED);
     gpio_set_direction(GPIO_BUTTON, GPIO_MODE_INPUT);
     gpio_set_pull_mode(GPIO_BUTTON, GPIO_PULLDOWN_ONLY);
     gpio_set_direction(GPIO_LED, GPIO_MODE_OUTPUT);
+    
+    uint delay = 100; // ms
+    uint msecs = 0;
+    uint8_t total_levels = 0;
+    uint8_t led_level = 0;
 
     while(true) {
         int level = gpio_get_level(GPIO_BUTTON);
-        gpio_set_level(GPIO_LED, level);
-
         if (level)
-            ESP_LOGI(TAG, "LED Turned ON");
-        else
-            ESP_LOGI(TAG, "LED Turned OFF");
+            total_levels++;
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        if ((total_levels == 2 && msecs >= 200 && msecs <= 900) || total_levels >= 10) {
+            led_level = !led_level;
+            gpio_set_level(GPIO_LED, led_level);
+        }
+
+        // if (level)
+        //     ESP_LOGI(TAG, "LED Turned ON");
+        // else
+        //     ESP_LOGI(TAG, "LED Turned OFF");
+
+        vTaskDelay(delay / portTICK_PERIOD_MS); // every 100ms
+        msecs += delay;
+        if (msecs >= 1000) {
+            total_levels = 0;
+            msecs = 0;
+        }
     }
 }
