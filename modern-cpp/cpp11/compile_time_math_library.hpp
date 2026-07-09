1a4361eadbddb6a5008c0d143253effe90d9dbf0
Ahsan Ullah <hatrakhan@gmail.com>
2026-07-09 23:00:52 +0500

    Restrict constexpr function returns to a single statement as per C++11 standard

--- modern-cpp/cpp11/compile_time_math_library.hpp
+++ modern-cpp/cpp11/compile_time_math_library.hpp
@@ -12,55 +12,59 @@
     · Add static_assert tests that validate results at compile time.
 */
 
-#include <algorithm>
 #include <stddef.h>
 
 namespace ctml {
 
 template<typename T>
 inline constexpr T factorial(T val) {
-    if (val <= 1)
-        return 1;
-
-    return val * factorial(val - 1);
+    return val <= 1 ? 1 : val * factorial(val - 1);
 }
 
 template<typename T>
 inline constexpr T fibonacci(T val) {
-    if (val == 0)
-        return 0;
-    if (val == 1)
-        return 1;
-
-    return fibonacci(val - 1) + fibonacci(val - 2);
+    return val == 0 ? 0 : 
+            (val == 1 ? 1 :
+                fibonacci(val - 1) + fibonacci(val - 2));
 }
 
 // Uses the Wheel Factorization method https://en.wikipedia.org/wiki/Wheel_factorization
 template<typename T>
+inline constexpr bool is_prime_helper(T val, size_t i) {
+    return i * i > val ? true :
+            (val % i == 0 || val % (i + 2) == 0) ? false :
+            is_prime_helper(val, i + 6);
+}
+
+template<typename T>
 inline constexpr bool is_prime(T val) {
-    if (val <= 1) return false;
-    if (val <= 3) return true;
-    if (val % 2 == 0 || val % 3 == 0) return false;
-
-    for (size_t i = 5; i * i <= val; i += 6) {
-        if (val % i == 0 || val % (i + 2) == 0)
-            return false;
-    }
-
-    return true;
+    return (val <= 3) ? (val > 1) :
+            (val % 2 == 0 || val % 3 == 0) ? false :
+            is_prime_helper(val, 5);
 }
 
 // Greatest Common Divisor
 template<typename T>
+inline constexpr T gcd_helper(T a, T b, T g) {
+    return g <= 0 ? 1 :
+            (a % g == 0 && b % g == 0) ? g :
+            gcd_helper(a, b, --g);
+}
+
+template<typename T>
+inline constexpr T max(T a, T b) {
+    return a > b ? a : b;
+}
+
+template<typename T>
+inline constexpr T min(T a, T b) {
+    return a < b ? a : b;
+}
+
+template<typename T>
 inline constexpr T gcd(T a, T b) {
-    if (!a || !b)
-        return std::max(a, b);
-
-    for (T i = std::min(a, b); i > 0; --i)
-        if (a % i == 0 && b % i == 0)
-            return i;
-
-    return 1;
+    return (!a || !b) ? max(a, b) :
+            gcd_helper(a, b, min(a, b));
 }
 
 }
